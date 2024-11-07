cce9c4ebe8a64a2156517dae84dfe1fe6fb5726d
Maksim Konstantinov <zgl6yxnt@yandex-team.ru>
2024-11-08 01:56:46 +0700

    Speedup shell calls.

--- src/main/java/io/eroshenkoam/xcresults/export/ExportProcessor.java
+++ src/main/java/io/eroshenkoam/xcresults/export/ExportProcessor.java
@@ -62,6 +62,8 @@ public class ExportProcessor {
 
     private static final String TEST_REF = "testsRef";
 
+    private static Boolean shouldUseLegacyMode = null;
+
     private final ObjectMapper mapper = new ObjectMapper()
             .disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
 
@@ -236,11 +238,15 @@ public class ExportProcessor {
     }
 
     private static boolean isLegacyMode() {
+        if (shouldUseLegacyMode != null) {
+            return shouldUseLegacyMode;
+        }
         try {
             final String output = readProcessOutputAsString(new ProcessBuilder("xcodebuild", "-version"));
             final String versionLine = output.split("\n")[0];
             final Version version = new Version(versionLine.replaceFirst("Xcode ", "").trim());
-            return version.getMajor() >= 16;
+            shouldUseLegacyMode = version.getMajor() >= 16;
+            return shouldUseLegacyMode;
         } catch (final Exception e) {
             return false;
         }
